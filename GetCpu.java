9319197396abe1273c7964536343847caa39a012
Jasi <Jaspreetsinghplayer@gmail.com>
2018-06-16 02:41:33 +0530

    optimized

--- GetCpu.java
+++ GetCpu.java
@@ -50,33 +50,21 @@ public class GetCpu{
         String rarc = "UNKNOWN";
 
         if (arch.equals("ARM")) {
-            rarc= "arm";
+            rarc = !is64 ? "arm" : "arm64";
         }else if (arch.equals("X86")){
-            rarc= "x86";
+	    rarc = !is64 ? "x86" : "x86_64";
         }else if (arch.equals("MIP")){
-            rarc= "mips";
+	    rarc = !is64 ? "mips" : "mips64";
         }
-
-        if (is64 == true){
-                // 64bit
-                if (rarc.equals("arm")) {
-                    rarc = "arm64";
-                }else if (rarc.equals("x86")){
-                    rarc = "x86_64";
-                }else if (rarc.equals("mips")){
-                    rarc = "mips64";
-                }
-        }
-	    
+    
 	Log.v("Jasi","CPU Detected :- "+rarc);
 
         Log.v("Jasi","Is 64 Bit CPU ? :- "+is64);
 
 	/* results arm,arm64,x86,x86_64,mips,mips64 (mips is very rare and mips64 is almost zero but google support dex2oat of it so i added)
 
-	i could have added x8664 like that on first check to make algo optimized and small but the name i used like x86_64 are the same name google uses and
-	same folder can be found in /data/dalvik-cache/x86_64 and  /data/app/app_pkg_name/oat/x86_64
-	as Uret Patcher deals with reversing so name change will make not working so name results are proper rather modifying it later*/
+	Name of CPU are proper same name according to google uses and same folder can be found in /data/dalvik-cache/x86_64 and 
+	/data/app/app_pkg_name/oat/x86_64 etc*/
 
 	return rarc;
     }
